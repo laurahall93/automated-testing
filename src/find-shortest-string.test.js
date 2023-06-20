60b71bf1424d55359f3028ed3f1672cffc6f9783
Laura Hall <podinjshmi1993@gmail.com>
2023-06-20 11:11:37 +0100

    added shortestStr file and test

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,7 @@
+const findShortestString = require("./find-shortest-string");
+
+test("findShortestString finds the shortest string in an array", () => {
+    expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+    expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+  });
+
